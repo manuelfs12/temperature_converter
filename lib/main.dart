7944b230fae0042c6d5dc111a5124a6b4e1563be
Manuel Figueroa <manuelfigueroa402@gmail.com>
2020-06-23 07:27:16 -0400

    added the remaining conversions and styled the app a bit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -25,12 +24,9 @@ class TemperatureConverter extends StatefulWidget {
   @override
   _TemperatureConverterState createState() => _TemperatureConverterState();
 
-
-
 }
 
 class _TemperatureConverterState extends State<TemperatureConverter> {
-
 
   String output;
   String output2;
@@ -43,9 +39,17 @@ class _TemperatureConverterState extends State<TemperatureConverter> {
     for (var i = 0; i < input.length; i++) {
       inputSubstring = input[i].toUpperCase();
       if (inputSubstring.contains('C')) {
-        print(input.substring(0, i));
-        output = (double.parse(input.substring(0, i)) + 273.15).toString() + ' K';
-        output2 = ((double.parse(input.substring(0, i)) * 9 / 5) + 32).toString() + ' F';
+        output = (double.parse(input.substring(0, i)) + 273.15).toStringAsFixed(2) + ' K';
+        output2 = ((double.parse(input.substring(0, i)) * 9 / 5) + 32).toStringAsFixed(2) + ' F';
+      } else if (inputSubstring.contains('F')) {
+        output = ((double.parse(input.substring(0, i)) - 32) * 5 / 9 + 273.15).toStringAsFixed(2) + ' K';
+        output2 = ((double.parse(input.substring(0, i)) - 32) * 5 / 9).toStringAsFixed(2) + ' C';
+      } else if (inputSubstring.contains('K')) {
+        output = (double.parse(input.substring(0, i)) - 273.15).toStringAsFixed(2) + ' C';
+        output2 = ((double.parse(input.substring(0, i)) - 273.15) * 9 / 5 + 32).toStringAsFixed(2) + ' F';
+      } else {
+        output = 'Invalid input';
+        output2 = output;
       }
     }
   }
@@ -68,11 +72,18 @@ class _TemperatureConverterState extends State<TemperatureConverter> {
 
       ),
       body: Container(
+        padding: EdgeInsets.only(top: 10.0),
         child: Column(
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.only(left: 20.0, right: 20.0),
               child: TextField(
+                textAlign: TextAlign.center,
+                decoration: InputDecoration(
+                  hintText: 'Eg: 150C',
+                  prefixIcon: Icon(Icons.ac_unit),
+                  suffixIcon: Icon(Icons.ac_unit),
+                ),
                 controller: inputController,
                 onSubmitted: (text) {
                   setState(() {
@@ -82,25 +93,25 @@ class _TemperatureConverterState extends State<TemperatureConverter> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.only(left: 20.0, top: 10.0, right: 20.0),
               child: Container(
                 alignment: Alignment.center,
                 padding: EdgeInsets.all(10.0),
                 color: Theme.of(context).primaryColor,
                 child: Text(
-                  output ?? '0',
+                  output ?? 'No input provided.',
                   style: TextStyle(color: Colors.white, fontSize: 20.0),
                 ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.only(left: 20.0, top: 10.0, right: 20.0),
               child: Container(
                 alignment: Alignment.center,
                 padding: EdgeInsets.all(10.0),
                 color: Theme.of(context).primaryColor,
                 child: Text(
-                  output2 ?? '0',
+                  output2 ?? 'No input provided.',
                   style: TextStyle(color: Colors.white, fontSize: 20.0),
                 ),
               ),
